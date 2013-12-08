f2b8a5a21800ec7321c2e1b6f20f63d3a34620c9
Abderahamne <teguia@local>
2013-12-08 15:29:11 +0100

    ADD column experated at

--- src/Ibw/JobeetBundle/Controller/JobController.php
+++ src/Ibw/JobeetBundle/Controller/JobController.php
@@ -24,7 +24,10 @@ class JobController extends Controller
         $em = $this->getDoctrine()->getManager();
 
         $entities = $em->getRepository('IbwJobeetBundle:Job')->findAll();
-
+        $query = $em->createQuery(
+            'SELECT j from IbwJobeetBundle:Job j WHERE j.expires_at > :date ')->
+                setParameter('date',date('Y-m-d H:i:s', time() - 86400 * 30));
+        $entities = $query->getResult();
         return $this->render('IbwJobeetBundle:Job:index.html.twig', array(
             'entities' => $entities,
         ));
